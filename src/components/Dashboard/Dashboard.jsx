02a65c73f95316c72ed49357701bc81157d0b2d6
Mehdi Nourollah <mehdinourollah@gmail.com>
2021-09-30 19:50:42 +0300

    minor

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-export default function Dashbourd() {
+export default function Dashboard() {
     return (
         <div>
-            Dashboard
+            <marquee>Hey</marquee>
         </div>
     )
 }
