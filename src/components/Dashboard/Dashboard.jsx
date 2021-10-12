29ee5874b660ec8b3347d5b4522664c5f37e6ad8
Mehdi Nourollah <mehdinourollah@gmail.com>
2021-10-12 19:50:26 +0300

    adding storybook and test units
    clean up bad practices in the project

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { useNavigate } from "react-router-dom";
+
+import { store } from '../../context';
+
+const Logout = () => {
+    const {globalState,dispatch} = useContext(store);
+    const navigate = useNavigate();
+    const logout = () => {
+        dispatch({type: 'logout'});
+        // sessionStorage.removeItem('token');
+        // navigate('/login');
+        
+    }
+    return (<button onClick={logout} className="border">logout</button>)
+
+
+}
 
 export default function Dashboard() {
     return (
-        <div>Dashboard Page</div>
+        <>
+            <div>Dashboard Page</div>
+            <Logout />
+        </>
     )
 }
+
