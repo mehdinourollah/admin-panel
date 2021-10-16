ec1b5e2107f22f27cbbeb398796b1aabfabc89b4
Mehdi Nourollah <mehdinourollah@gmail.com>
2021-10-16 22:43:07 +0300

    request direct added

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import 'antd/dist/antd.css';
 import App from './components/App'
 import { StateProvider } from './context'
 
+fetch('https://api.laroza.dev/').then(res => res.text()).then(d => console.log(d))
+
 ReactDOM.render(
     <React.StrictMode>
         <StateProvider>
