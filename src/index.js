4b9a47974d9a0f3ed3bdf0dfc6c00d74e2cd62ee
Mehdi Nourollah <mehdinourollah@gmail.com>
2021-10-16 22:49:36 +0300

    minor2

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import 'antd/dist/antd.css';
 import App from './components/App'
 import { StateProvider } from './context'
 console.log('123')
-fetch('https://api.laroza.dev/').then(res => res.text()).then(d => console.log(d))
+fetch('https://api.laroza.dev/', { headers: { 'Access-Control-Allow-Origin': '*','mode':'no-cors' } }).then(res => res.text()).then(d => console.log(d))
 console.log('456')
 ReactDOM.render(
     <React.StrictMode>
