3ceb3aa408a1d8f1497a2253f44617bf5388c646
Mehdi Nourollah <mehdinourollah@gmail.com>
2021-10-16 22:46:29 +0300

    minor

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ import './index.css'
 import 'antd/dist/antd.css';
 import App from './components/App'
 import { StateProvider } from './context'
-
+console.log('123')
 fetch('https://api.laroza.dev/').then(res => res.text()).then(d => console.log(d))
-
+console.log('456')
 ReactDOM.render(
     <React.StrictMode>
         <StateProvider>
